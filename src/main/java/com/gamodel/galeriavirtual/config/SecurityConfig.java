a0f0286bf95317bbaa7f6efe3c4c5f95f6d04a73
andresramirez31 <andres.ramirez31@udea.edu.co>
2024-10-23 19:10:34 -0500

    Primer API

---
+++ src/main/java/com/gamodel/galeriavirtual/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.gamodel.galeriavirtual.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/gamodel/galeriavirtual/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.gamodel.galeriavirtual.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gamodel.galeriavirtual.dto.LoginRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("Api/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+
+        System.out.println("Received login request: ");
+
+        if ("user@example.com".equals(loginRequest.getEmail()) && "password123".equals(loginRequest.getPassword())) {
+            return ResponseEntity.ok("Login exitoso");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales invalidas");
+        }
+
+    }
+
+}
